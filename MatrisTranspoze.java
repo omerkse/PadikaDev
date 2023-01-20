037cad0c242a33f817c64b0a48d93c41ff45263b
omerkse <117233659+omerkse@users.noreply.github.com>
2023-01-20 18:12:32 +0300

    www.patika.dev
    Patika Dev Pratik Ödev

---
+++ MatrisTranspoze.java
@@ -0,0 +1,31 @@
+public class MatrisTranspoze {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1, 7, 3},
+                {2, 5, 4},
+                {10, 8, 9},
+        };
+
+        int[][] transpose = new int[matrix[0].length][matrix.length];
+
+        System.out.println("Matris: ");
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("Transpose: ");
+
+        for (int i = 0; i < transpose.length; i++) {
+            for (int j = 0; j < transpose[i].length; j++) {
+                transpose[i][j] = matrix[j][i];
+                System.out.print(transpose[i][j] + " ");
+            }
+            System.out.println();
+        }
+        }
+    }
+
